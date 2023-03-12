d15a3ab54885f57e019a055d1d3a5f304769160f
dya-only <dyacode@proton.me>
2023-03-12 14:09:56 +0900

    connect db

--- server/src/routes/api/users/auth/new.auth.ts
+++ server/src/routes/api/users/auth/new.auth.ts
@@ -1,18 +1,23 @@
 import express from "express"
-import { Database } from 'sqlite3'
+import {Database, OPEN_READWRITE} from 'sqlite3'
 
+const db = new Database('db/user.db', OPEN_READWRITE, (err) => {
+  if (err) console.error(err.message)
+  else console.log('Connected to the database.')
+})
 const router = express.Router()
 const log = console.log
 
 router.route('/').get(async (req, res) => {
-  const { run } = new Database('db/user.db')
   const params = req.query as any
 
   // CREATE DB TABLE
-  run('CREATE TABLE user(idx integer primary key autoincrement, id text unique, pw text, name text, tag text)')
+  db.run('CREATE TABLE IF NOT EXISTS users(idx integer primary key autoincrement, id text unique, pw text, name text, tag text)')
 
   // log(`id: ${params.id}\npw: ${params.pw}\nname: ${params.name}\ntag: ${params.tag}`)
   res.json({ id: params.id, pw: params.pw, name: params.name, tag: params.tag, message: 'create successful' })
+
+  db.close()
 })
 
 export default router
