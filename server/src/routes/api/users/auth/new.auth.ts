1a983ae2218c9014f9f58f25359dbcb35ca89e37
dya-only <dyacode@proton.me>
2023-03-12 20:07:27 +0900

    db insert fix

--- server/src/routes/api/users/auth/new.auth.ts
+++ server/src/routes/api/users/auth/new.auth.ts
@@ -12,9 +12,14 @@ router.route('/').get(async (req, res) => {
   const params = req.query as any
 
   // CREATE DB TABLE
-  db.run('CREATE IF NOT EXISTS TABLE users(idx integer primary key autoincrement, id text unique, pw text, name text, tag text)')
+  db.run('CREATE TABLE IF NOT EXISTS users(idx integer primary key autoincrement, id text unique, pw text, name text, tag text)')
 
-  // log(`id: ${params.id}\npw: ${params.pw}\nname: ${params.name}\ntag: ${params.tag}`)
+  db.run(`INSERT INTO users(id, pw, name, tag) VALUES(\'${params.id}\', \'${params.pw}\', \'${params.name}\', \'${params.tag}\')`, (err) => {
+    if (err) return console.log(err.message)
+
+    console.log(`create account [${params.name}] successful`)
+  })
+
   res.json({ id: params.id, pw: params.pw, name: params.name, tag: params.tag, message: 'create successful' })
 
   db.close()
